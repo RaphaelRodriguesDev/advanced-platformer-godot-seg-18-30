65ed8d39b4f7607dc65b8c7d5d85ccf14317b080
RaphaelRodriguesDev <professor.dev.raphael@gmail.com>
2025-06-02 18:31:01 -0300

    Script de movimentacao horizontal 2

--- scripts/enemy/enemy_template.gd
+++ scripts/enemy/enemy_template.gd
@@ -15,3 +15,17 @@ var player_ref: Player = null
 export(int) var speed
 export(int) var gravity_speed
 export(int) var proximity_threshold
+
+func _physics_process(delta: float) -> void:
+	gravity(delta)
+	move_behavior()
+	
+func gravity(delta: float) -> void:
+	velocity.y += delta * gravity_speed
+	
+	
+func move_behavior() -> void:
+	if player_ref != null:
+		var distance: Vector2 = player_ref.global_position - global_position
+		var direction: Vector2 = distance.normalized()
+	
