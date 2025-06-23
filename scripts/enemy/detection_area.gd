a76959b2f79583655433bed7139acae03642b1e5
RaphaelRodriguesDev <professor.dev.raphael@gmail.com>
2025-06-23 18:25:09 -0300

    Adicionando projeto completo em godot 3 e godot 4

--- scripts/enemy/detection_area.gd
+++ scripts/enemy/detection_area.gd
@@ -1,10 +1,21 @@
 extends Area2D
 class_name DetectionArea
 
-export(NodePath) onready var enemy = get_node(enemy) as KinematicBody2D
+export(NodePath) var enemy_path
+onready var enemy = get_node(enemy_path) as KinematicBody2D
+
+func _ready():
+	if not enemy:
+		printerr("Erro: O nó 'enemy' não foi configurado corretamente ou não é um KinematicBody2D. Caminho: ", enemy_path)
 
 func on_body_entered(body: Player) -> void:
-	enemy.player_ref = body
+	if enemy:
+		enemy.player_ref = body
+	else:
+		printerr("Erro: Não foi possível definir player_ref, enemy é null")
 
 func on_body_exited(_body: Player) -> void:
-	enemy.player_ref = null
+	if enemy:
+		enemy.player_ref = null
+	else:
+		printerr("Erro: Não foi possível limpar player_ref, enemy é null")
--- scripts/enemy/enemy_texture.gd
+++ scripts/enemy/enemy_texture.gd
@@ -1,12 +1,16 @@
 extends Sprite
 class_name EnemyTexture
 
-export (NodePath) onready var attack_area_collision = get_node(attack_area_collision) as CollisionShape2D
-export (NodePath) onready var animation = get_node(animation) as AnimationPlayer
-export (NodePath) onready var enemy = get_node(enemy) as KinematicBody2D
+export (NodePath) var attack_area_collision_path
+export (NodePath) var animation_path
+export (NodePath) var enemy_path
+
+onready var attack_area_collision = get_node(attack_area_collision_path) as CollisionShape2D
+onready var animation = get_node(animation_path) as AnimationPlayer
+onready var enemy = get_node(enemy_path) as KinematicBody2D
 
 func animate(_velocity: Vector2) -> void:
 	pass
 
 func on_animation_finished(_anim_name: String):
-	pass # Replace with function body.
+	pass
